9e72d2a463bb2c5ebb91148019ced77d20bee09c
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-16 14:26:09 -0700

    C Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+/** main 
+*/
 /**
 *programe
-*is like
-*a building
 */
 
 
 int main(void)
 {
+	
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
+
 }
