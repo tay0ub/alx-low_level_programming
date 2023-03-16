37033e5f520b0300e80d2605de88dbdfe704141a
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-16 15:03:25 -0700

    C Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,14 @@
+/*
+ * file : 5-printf.c
+ */
+
 #include <stdio.h>
+
+/**
+ * main - print
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
