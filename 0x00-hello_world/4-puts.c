bee76070f605c3519ded9921cadc9142c8f5f8d4
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-16 14:56:45 -0700

    C Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /*
  * File : 4-puts.c
- */ 
+*/ 
 #include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
