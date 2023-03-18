1d5723bbb7df176d64d0091aca85d297fc3fd9bf
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-17 18:45:10 -0700

    C-variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,14 @@
  */
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
-	}
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+	putchar('\n');
+	return (0);
+}
