f4bc8958188f80c196d9c171e488699a97ccaccf
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-21 19:44:44 -0700

    C functions_nested_loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+ * task 15
+ */
+
+/**
+ * main - print sum of even valued fibonacci sequence 
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, fs;
+	float tsum;
+
+	while (1)
+	{
+		fsu = f1 + f2;
+		if (fs > 4000000)
+			break;
+
+		if ((fs % 2) == 0)
+			tsum += fs;
+
+		f1 = f2;
+		f2 = fs;
+	}
+	printf("%.0f\n", tsum);
+
+	return (0);
+}
