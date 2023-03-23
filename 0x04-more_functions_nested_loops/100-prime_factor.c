01ebbb5971340d2077bf1070379a01f32a7b05fa
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-23 15:23:37 -0700

    print the prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+ * task 11
+ */
+
+/**
+ * main - print the largest prime factor of the number
+ *        612852475143.
+ * Return: 0
+ */
+
+int main(void)
+{
+	long a = 612852475143, b;
+
+	while (b < (a / 2))
+	{
+		if ((a % 2) == 0)
+		{
+			a /= 2;
+			continue;
+		}
+
+		for (b = 3; b < (a / 2); b += 2)
+		{
+			if ((a % b) == 0)
+				a /= b;
+		}
+	}
+
+	printf("%ld\n", a);
+
+	return (0);
+}
