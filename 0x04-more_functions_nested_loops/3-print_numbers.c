e59e0afc8c1cd722736a2ce10e564abd5f811f53
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-23 14:22:33 -0700

    print number 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * task 3
+ */
+
+/**
+ * print_numbers - print numbers from 0 to 9.
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar((n % 10) + '0');
+
+	_ptuchar('\n');
+}
