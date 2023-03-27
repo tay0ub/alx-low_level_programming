0948f3c579f642ec49b687b049ed548e32aaab92
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-27 15:35:45 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * task 2
+ */
+
+/**
+ * _strlen - return length of a string
+ * @s: the string
+ * Return : the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
