e260420e53a6d85a0f214e6737830297a6cf6a41
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-29 16:50:20 -0700

    concatenates two strings with length

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/*
+ * task 1
+ */
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: the string wich will be concatenates into.
+ * @src: the string wich will be concatenated in @dest.
+ * @n: length.
+ * Return: concatenated string.
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	char *str = dest;
+	int a = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+	}
+
+	while (*src != '\0' && a<n)
+	{
+		*str = *src;
+		str++;
+		src++;
+		a++;
+	}
+	*str = '\0';
+
+	return (dest);
+}
