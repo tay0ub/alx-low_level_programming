5d1d33c120c7645e2eb49a95e3dab05ddd349f42
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-29 14:45:11 -0700

    Concatenates 2 string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, dest_len = 0;
+	int index, dest_len = 0;
 
-	while (dest[i++])
+	while (dest[index++])
 		dest_len++;
 
-	for (i = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
