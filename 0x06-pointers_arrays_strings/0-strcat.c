60fe676a5d9542cded294250b408fe8df136b65f
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-29 15:01:33 -0700

    Concatenates 2 string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/*
+ * task 0
+ */
+
+/**
+ * _strcat - concatenates string @src include null
+ *           byte to the end of string @dest
+ * @dest:string to concanate uppon
+ * @src:source string
+ * Return:dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *str = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	
+	return (str);
+}
