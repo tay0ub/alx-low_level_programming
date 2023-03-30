2dfad82e26c681dc60a639293e9e60001fb3cac1
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-30 15:16:00 -0700

    encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/*
+ * task 7
+ */
+
+/**
+ * leet - encodes a string in 1337
+ * @s: the string
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int e, f;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (e = 0; s[e] != '\0'; e++)
+	{
+		for (f = 0; f < 10; f++)
+		{
+			if (s[e] == a[e])
+			{
+				s[e] = b[f];
+			}
+		}
+	}
+
+	return (s);
+}
