da1da2d809756713e83650c25917b2650a5d3988
ayoub ti <ayoub.ti20223@gmail.com>
2023-03-30 15:24:57 -0700

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+include "main.h"
+
+/*
+ * task 9
+ */
+
+/**
+ * print_number - print an integer
+ * @n: number to print
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -n;
+	} else
+	{
+		a = n;
+	}
+
+	if (a / 10)
+		print_number(a / 10);
+
+	_putchar((a % 10) + '0');
+}
