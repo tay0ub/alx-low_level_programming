c93440bf355f275a95ce63bc947b73234c2b96d8
ayoub ti <ayoub.ti20223@gmail.com>
2023-04-05 15:30:28 -0700

    fix

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,7 +12,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int prime(int a, int n);
-int _string(char *s, int x, int len);
-int _length(int y, char *s);
 
 #endif
